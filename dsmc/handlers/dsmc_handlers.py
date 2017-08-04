edc80157c5f484b5f3030ca39e27111c42dd5ed5
Johan Hermansson <johan.hermansson@medsci.uu.se>
2017-08-04 17:12:20 +0200

    Support reupload of missing files

--- dsmc/handlers/dsmc_handlers.py
+++ dsmc/handlers/dsmc_handlers.py
@@ -1,9 +1,10 @@
-
 import json
 import logging
 import os
 import datetime
 import uuid
+import subprocess
+import re 
 
 from arteria.exceptions import ArteriaUsageException
 from arteria.web.state import State
@@ -45,6 +46,7 @@ class VersionHandler(BaseDsmcHandler):
 
 
 class ReuploadHandler(BaseDsmcHandler):
+    # TODO: Refactor out
     @staticmethod
     def _validate_runfolder_exists(runfolder, monitored_dir):
         if os.path.isdir(monitored_dir):
@@ -67,90 +69,150 @@ class ReuploadHandler(BaseDsmcHandler):
 # need to be able to launch it on Irma as well. Otherwise the service will 
 # just have to call out to the scripts. 
 
+
+    # TODO: Check so that we are not making any mistakes with 
     def post(self, runfolder_archive): 
-        pass
-'''        monitored_dir = self.config["monitored_directory"]
+        monitored_dir = self.config["monitored_directory"]
 
         if not UploadHandler._validate_runfolder_exists(runfolder_archive, monitored_dir):
             raise ArteriaUsageException("{} is not found under {}!".format(runfolder_archive, monitored_dir))            
 
         path_to_runfolder = os.path.join(monitored_dir, runfolder_archive)
+        uniq_id = str(uuid.uuid4())        
+        dsmc_log_dir = self.config["dsmc_log_directory"]
 
-        ## pdc-descr
+        if not UploadHandler._is_valid_log_dir(dsmc_log_dir):
+            raise ArteriaUsageException("{} is not a directory!".format(dsmc_log_dir))
 
-        if [ $# -eq 0 ]; then
-        echo "Missing argument. Must give 1) the runfolder/archive (not path, and without trailing /) name to check for."
-        exit 1
-        fi 
-
-        RUNFOLDER=$1
-
-        dsmc q ar /proj/ngi2016001/incoming/${RUNFOLDER} | grep "/proj/ngi2016001/incoming" | awk '{print $3" "$NF}'
+        dsmc_log_file = "{}/dsmc_{}_{}-{}".format(dsmc_log_dir,
+                                                      runfolder_archive,
+                                                      uniq_id,
+        #                                              description,
+                                                      datetime.datetime.now().isoformat())            
 
 
-        ## pdc-diff
+        # Problem here is that we do not want to queue all the DSMC jobs, as that would become messy
+        # Because we don't want to respond each time. 
+        # I.e. the fetch of descr, and the diff commands we want to execute directly. 
+        # Only the last reupload should be done with the queue. 
 
-        if [ $# -le 1 ]; then
-        echo "Missing argument. Must give 1) the path to the runfolder/archive to check for, 2) description of archived path."
-        exit 1
-        fi 
+        # Step 1 - fetch the description of the last uploaded version of this archive
 
-        RFPATH=`realpath $1`
-        RUNFOLDER=`basename ${RFPATH}`
-        RFPARENT=`dirname ${RFPATH}`
-        DESCR=$2
+        #dsmc q ar /proj/ngi2016001/incoming/${RUNFOLDER} | grep "/proj/ngi2016001/incoming" | awk '{print $3" "$NF}'
 
-        # Step 1, get filelist from PDC
-        dsmc q ar /proj/ngi2016001/incoming/${RUNFOLDER}/ -subdir=yes -description=${DESCR} > ${RUNFOLDER}.pdc_raw
-        grep "/proj/ngi2016001" ${RUNFOLDER}.pdc_raw | awk '{print $1}'| sed 's/,//g' > ${RUNFOLDER}.pdc_bytes
-        grep "/proj/ngi2016001" ${RUNFOLDER}.pdc_raw | awk -F " Never " '{print $1}' | sed -n 's,.*/proj/ngi2016001/incoming,/proj/ngi2016001/incoming,p' > ${RUNFOLDER}.pdc_files
-        paste -d " " ${RUNFOLDER}.pdc_files ${RUNFOLDER}.pdc_bytes | sort > ${RUNFOLDER}.pdc_list
-        #rm pdc-bytes pdc-files ${RUNFOLDER}.pdc_raw
 
-        # Step 2, get expected filelist from us 
-        find -L ${RFPATH}/ -printf "%p %s\n" > ${RUNFOLDER}.orig_list
-        # Convert paths to original format used when uploading 
-        sed -i "s,${RFPARENT},/proj/ngi2016001/incoming,g" ${RUNFOLDER}.orig_list 
-        # remove first line, the parent dir, which is not included in pdc output
-        sed '1d' ${RUNFOLDER}.orig_list > ${RUNFOLDER}.tmp_list 
-        sort ${RUNFOLDER}.tmp_list > ${RUNFOLDER}.orig_list_sorted
-        mv ${RUNFOLDER}.orig_list_sorted ${RUNFOLDER}.orig_list
-        rm ${RUNFOLDER}.tmp_list
+        #cmd = "dsmc q ar {}".format(path_to_runfolder)
+        cmd = "cat /tmp/apa/descr-list.txt"
+        # TODO: Check that process completed successfully
+        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        # TODO: This can deadlock according to https://docs.python.org/2/library/subprocess.html
+        retval = p.wait()
 
-        if ! diff ${RUNFOLDER}.orig_list ${RUNFOLDER}.pdc_list > ${RUNFOLDER}.diff ; then 
-                echo "Filelist and/or number of bytes between ${RFPATH} and PDC differs!"
-                echo "See ${RUNFOLDER}.diff for details."
-                exit 1
-        else
-                echo "Filelist and number of bytes are identical between ${RFPATH} and copy on PDC."
-                exit 0
-        fi
+        #Accessing as node: SLLUPNGI_TEST
+        #13              Size  Archive Date - Time    File - Expires on - Description
+        #14              ----  -------------------    -------------------------------
+        #15          4,096  B  01/10/2017 15:16:26    /data/mm-xart002/runfolders/johanhe_test_150821_M00485_0220_000000000-AG2UJ_archive Never #bce2592a-3bcb-4d5d-acbc-3170469ff23a
+        #16          4,096  B  01/10/2017 16:09:42    /data/mm-xart002/runfolders/johanhe_test_150821_M00485_0220_000000000-AG2UJ_archive Never #d1b51671-25b8-4bb1-85bf-f9d82082f0ef
+        
+        #my_string.split('\n')
+        my_lines = p.stdout.readlines()
+        #my_match = os.path.join(monitored_dir, runfolder_archive)
+        log.debug("My lines: {}".format(my_lines))
+        #log.debug("My match: {}".format(my_match))
+        matched_lines = [line.strip() for line in my_lines if path_to_runfolder in line]
+        log.debug("Matched lines: {}".format(matched_lines))
+        latest_upload = matched_lines[-1:][0]
+        latest_descr = latest_upload.split()[-1:][0]
+        log.debug("Last upload: {}".format(latest_upload))
+        log.debug("Last descr: {}".format(latest_descr))
 
-        ## pdc-upload-missing
+        #status_end_point = "{0}://{1}{2}".format(
+        #    self.request.protocol,
+        #    self.request.host,
+        #    self.reverse_url("status", job_id))
 
-        if [ $# -le 2 ]; then
-        echo "Missing argument. Must give 1) the runfolder/archive (not path, and without trailing /) name to upload for, 2) the description to re-use, 3) path to file containing the missing files to upload."
-        exit 1
-        fi
+        # Step 2 - check the difference of the uploaded version vs the local archive
 
-        RUNFOLDER=$1
-        DESCR=$2
-        DIFF=$3
+        # Step 1, get filelist from PDC      
+        #cmd = "dsmc q ar {} -subdir=yes -description={}".format(path_to_runfolder, latest_descr)
+        cmd = "cat /tmp/apa/runfolder-content.txt"
+        # TODO: Check that process completed successfully
+        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        # TODO: This can deadlock according to https://docs.python.org/2/library/subprocess.html
+        retval = p.wait()
+        my_lines = p.stdout.readlines()
+        log.debug("RAW uploaded data: {}".format(my_lines))
 
-        if [ `grep ${RUNFOLDER} ${DIFF} | wc -l` -eq 0 ]; then 
-                echo "No mention of ${RUNFOLDER} in ${DIFF}. Did you enter correct arguments?"
-                exit 1
-        fi 
+        # Take out the bytes and the filename from the output
+        # convert raw output to bytes - first field in matched_lines
+        #
+        # Get the lines containing the path. Then take out the field nr 1 and 5
+        matched_lines = [line.strip() for line in my_lines if path_to_runfolder in line]
+        log.debug("RAW matched lines: {}".format(matched_lines))
 
-        FILELIST=${RUNFOLDER}.files_to_upload
+        # FIXME: build up this with dicts instead - makes it easier to compare. 
+        # NB uploaded list contains folders as well, but when we check local content
+        # we only look at the files, and ignore the folders.
+        uploaded_files = {} #[line for line in matched_lines]
 
-        # Remove first and last column, preserving eventual spaces in paths, and then in the end
-        # enclose the strings (paths to files for uploading) in quotes. 
-        grep "/proj/ngi2016001/incoming" ${DIFF} | cut -d' ' -f2- | rev | cut -d ' ' -f2- | rev | sed -e 's/^/"/; s/$/"/' > ${FILELIST}
+        for line in matched_lines: 
+            elements = line.split()
+            byte_size = elements[0].replace(",", "")
+            filename = elements[4]
+            # Check so that the key doesn't exist first 
+            uploaded_files[filename] = byte_size
 
-        uniq_id = str(uuid.uuid4())
-        cmd = "dsmc archive {}/ -subdir=yes -description={}".format(path_to_runfolder, uniq_id)
-        job_id = self.runner_service.start(cmd, nbr_of_cores=1, run_dir="/tmp", stdout="/tmp/stdout", stderr="/tmp/stderr")
+        
+        log.debug("Uploaded files: {}".format(uploaded_files))
+
+        # Then, get the expected filelist from us
+        # FIXME: Use a dict instead 
+        local_files = {}
+        for root, directories, filenames in os.walk(path_to_runfolder):
+            for filename in filenames:
+                full_path = os.path.join(root, filename)
+                local_size = os.path.getsize(full_path)
+                local_files[full_path] = str(local_size)
+        
+        log.debug("Local files {}".format(local_files))
+
+        # Sort both files - then compare them. We do not need to sort them if we are using dicts!
+        # We consider local files to be the truth - obviously. So if there are more data uploaded 
+        # than present locally we ignore it. 
+        reupload_files = []
+        for k, v in local_files.iteritems(): 
+            if k in uploaded_files: 
+                log.debug("Local file has been uploaded {}".format(k))
+
+                if v != uploaded_files[k]: 
+                    log.info("::: ERROR ::: Local file size {} doesn't match remote file size {} for file {}".format(v, uploaded_files[k], k))
+                    reupload_files.append(k)
+                else: 
+                    log.debug("Local file size matches uploaded file size")
+            else: 
+                log.info("::: ERROR ::: Local file has NOT been uploaded {}".format(k))
+                reupload_files.append(k)
+
+        # Step 3 - upload the missing files with the previous description
+        if reupload_files: 
+            log.info("Will now reupload the following files: {}".format(reupload_files))
+
+            # TODO: Better path? 
+            dsmc_reupload = os.path.join("/tmp", "dsmc-reupload-{}".format(uniq_id))
+            with open(dsmc_reupload, 'wa') as f:
+                for r in reupload_files:  
+                    f.write('"{}"\n'.format(r))
+
+            log.debug("Written files to reupload to {}".format(dsmc_reupload))
+
+            #dsmc archive -descr=${DESCR} -filelist=${FILELIST} 2>&1 | tee `pwd`/${FILELIST}.tsm_log
+            cmdtwo = "dsmc archive -filelist={} -description={}".format(dsmc_reupload, latest_descr)
+            cmd = "echo {}".format(cmdtwo)
+            log.debug("Running command {}".format(cmdtwo))
+            job_id = self.runner_service.start(cmd, nbr_of_cores=1, run_dir=monitored_dir, stdout=dsmc_log_file, stderr=dsmc_log_file)
+
+        else: 
+            log.debug("Nothing to do - everything already uploaded.")
 
         status_end_point = "{0}://{1}{2}".format(
             self.request.protocol,
@@ -161,11 +223,18 @@ class ReuploadHandler(BaseDsmcHandler):
             "job_id": job_id,
             "service_version": version,
             "link": status_end_point,
-            "state": State.STARTED}#,
-            #"dsmc_log": dsmc_log_file}
+            "state": State.STARTED,
+            "dsmc_log": dsmc_log_file}
 
         self.set_status(202, reason="started processing")
-        self.write_object(response_data)
+        self.write_object(response_data)     
+
+'''        
+
+        uniq_id = str(uuid.uuid4())
+        cmd = "dsmc archive {}/ -subdir=yes -description={}".format(path_to_runfolder, uniq_id)
+        job_id = self.runner_service.start(cmd, nbr_of_cores=1, run_dir="/tmp", stdout="/tmp/stdout", stderr="/tmp/stderr")
+
 '''
 
 class UploadHandler(BaseDsmcHandler):
@@ -227,7 +296,6 @@ class UploadHandler(BaseDsmcHandler):
         #                                              description,
                                                       datetime.datetime.now().isoformat())
 
-        #cmd = " ".join(["md5sum -c", path_to_md5_sum_file])
        # cmd = "export DSM_LOG={} && dsmc archive {} -subdir=yes -desc={}".format(dsmc_log_file,
        #                                                                          runfolder,
        #                                                                          description)
@@ -239,12 +307,6 @@ class UploadHandler(BaseDsmcHandler):
         #cmd = "echo 'ANS1809W ANS2000W Test run started.' && echo ANS9999W && echo ANS1809W && exit 8" #false
         cmd = "echo 'ANS1809W Test run started.' && echo ANS1809W && exit 8"
         job_id = self.runner_service.start(cmd, nbr_of_cores=1, run_dir=monitored_dir, stdout=dsmc_log_file, stderr=dsmc_log_file)
-
-        #job_id = self.runner_service.start(cmd,
-        #                                   nbr_of_cores=1,
-        #                                   run_dir=monitored_dir,
-        #                                   stdout=dsmc_log_file,
-        #                                   stderr=dsmc_log_file)
 
         status_end_point = "{0}://{1}{2}".format(
             self.request.protocol,
